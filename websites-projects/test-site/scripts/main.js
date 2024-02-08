d3f3be74d161c909110e4c4fb619281a8f23950b
Torraspedro <156122655+Torraspedro@users.noreply.github.com>
2024-02-07 21:27:02 -0300

    Add files via upload

---
+++ websites-projects/test-site/scripts/main.js
@@ -0,0 +1,35 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/cat.jpg") {
+    myImage.setAttribute("src", "images/cat3.jpg");
+  } else {
+    myImage.setAttribute("src", "images/cat.jpg");
+  }
+};
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `cats are cool, ${storedName}`;
+}
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `cats are cool, ${myName}`;
+  }
+}
+
+myButton.onclick = () => {
+  setUserName();
+};
+
+
